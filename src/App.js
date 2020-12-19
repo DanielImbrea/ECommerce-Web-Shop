82014e13afb1e9089d3089a876fc2acc5e541c6d
Daniel Imbrea <daniel@sourcetask.com>
2020-12-20 00:09:28 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
-import Products from "./components/Products/Products";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar } from "./components";
 export const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  // async function e ca si cum ai folosi try and catch, doar ca codul arata mult mai clean
+  const fetchProducts = async () => {
+    //   asteptam un api de la commerce.products.list
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    // const cart = await commerce.cart.retrieve();
+    // setCart(cart);
+    // or
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+  console.log(products);
   return (
     <div>
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,24 +9,24 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
-import classes from "*.module.css";
-const Navbar = () => {
+import useStyles from "./styles";
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit<">
         <Typography variant="h6" className={classes.title} color="inherit">
           <img
-            src={}
+            src="#"
             alt="Commerce.js"
             height="25px"
             className={classes.image}
           />
-          Commerce.js
         </Typography>
         <div className={classes.grow} />
         <div className={classes.button}>
           <IconButton aria-label="Show cart items" color="inherit">
-            <Badge badgeContent={2} color="secondary">
+            <Badge badgeContent={totalItems} color="secondary">
               <ShoppingCart />
             </Badge>
           </IconButton>
